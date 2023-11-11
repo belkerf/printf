b90f0dd7aed8329ee55dbc250d9ea77c88668ac5
belkerf <jbelkerf@gmail.com>
2023-11-11 18:04:22 +0100

    creating the _printf file

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _printf - the copy made function of printf
+ * @format : the format we give to printf
+ * Return: it return the number of characters that are printed.
+ */
+int _printf(const char *format, ...)
+{
+}
