b53b3e7a380ffaeabe1ff67f111bcee645804cc0
belkerf <belkerf@hotmail.com>
2024-10-01 22:16:03 +0100

    editing_print_char

--- print_char.c
+++ print_char.c
@@ -11,7 +11,8 @@
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-void	print_char(char c)
+int	print_char(char c)
 {
 	write(1, &c,1);
+	return (1);
 }
